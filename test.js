e9403ebbd9dcb14a7812cbada06d5dac15d6f1d6
StephanyKleen <stephanykleen@gmail.com>
2023-07-18 16:35:38 -0700

    Solving callback issues where the class stays bounded to this and passes the value when using a setTimeout function style method

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-// Your code here
+const { Employee } = require('./employee')
+
+const employee = new Employee("John Wick", "Dog Lover")
+
+
+/*-------------------------------------------------------*/
+//Exercise 2
+let bound = employee.sayName.bind(employee)
+
+setTimeout(bound, 2000)
+
+//Exercise 3
+
+
+let occupation = employee.sayOccupation.bind(employee);
+
+setTimeout(occupation, 3000)
